0646ed82e0d84627beb3bc1acb855ad0b79cd969
Finkch <skyler@thealdersons.org>
2024-11-26 12:30:48 -0800

    Runs CD finder and updated todo.

--- main.py
+++ main.py
@@ -5,21 +5,37 @@
 # According to ChatGPT, which says according to percolation theory, the answer is about pho = 0.59.
 # I prefer my coffee drip brewed, so let's find out experimentally.
 #       Wow...that was a bad joke.
+#
+# First real simulation has finished!
 
-from experiment import Experiment
+# Todo:
+#   * Get trials to cutoff early if they are very confident they are not infinite
+#   * Improve CDFinder end condition
+#   *   - End when step is sufficiently small?
+#   *   - End when deviation between last n experiments is sufficiently small?
+#   * Think about deltas in experiment: don't cutoff early when infinite, rather delta proportional to #infinite?
+#   * Add another bit (or repurpose 1) to code if cell in in RQ to avoid lengthy 'in' check
+#   * Write key results to file 
+#   * Make graphs of reveals vs. density
+#       - Reveals vs. density (trial)
+#       - Mean/median reveals vs. density (experiment)
+#       - Max reveals vs. density (experiment)
+#       - Phase diagram cutoff vs. trials (CD finer)
+
+from critical import CriticalDensity
 
 def main():
     
-    trials = int(1e2)
+    experiments = int(1e2)
+    trials = int(1e3)
     rho = 3 / 5
     cutoff = 1e5
+    step = 0.3
+    alpha = 0.65
 
-    exp: Experiment = Experiment(rho, cutoff, trials)
+    finder = CriticalDensity(experiments, trials, rho, cutoff, step, alpha)
 
-    exp.begin(quiet = False)
-
-    print(exp)
-    
+    print(finder.find(False))
 
 
 main()
