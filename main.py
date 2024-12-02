e90d3774128b82a40228bad5a31b2b339d7f9a69
Finkch <skyler@thealdersons.org>
2024-12-01 16:47:27 -0800

    Updated todo, changed execution path to rho finder

--- main.py
+++ main.py
@@ -7,6 +7,7 @@
 #       Wow...that was a bad joke.
 #
 # First real simulation has finished!
+#   rho_critical ~= 0.1022 mines/cell
 
 # Todo:
 #   * Get trials to cutoff early if they are very confident they are not infinite
@@ -22,12 +23,22 @@
 #       - Mean/median reveals vs. density (experiment)
 #       - Max reveals vs. density (experiment)
 #       - Phase diagram cutoff vs. trials (CD finer)
-#   * Figure out why rho_critical keeps changing and get a more true value
-#       - Visual representation of Minesweeper state
-#       - Be able to walk through Minesweeper states
+#   x   * Figure out why rho_critical keeps changing and get a more true value
+#   x       - Visual representation of Minesweeper state
+#   x       - Be able to walk through Minesweeper states
 
 # Dangit. My performance gains caused rho_critical to go from 0.1034 to ~0.2
 # In fact, oddly close to doubling. Huh...
+#   ...
+# Oki, doubling was just a coincidence. Turns out the clever "torq" optimisation was
+# incorrect. It assumed that it only needed to check newly added neighbours rather than
+# all adjacent cells. Here is a counter scenario:
+#   Imagine two adjacent cells, A and B, where A is adjacent to a mine but not B. A is revealed 
+#   first and adds neighbours, all to flag 0 meaning not in reveal queue. Then B is revealed but even
+#   though it is zero value (not flag 4), its neighbours already exist so it cannot add them to torq.
+# Something similar happened in reverse if B then A causing a cell to revealed that should not have been.
+# This is another reason why we use BFS instead of DFS since DFS would reveal cells that maybe shouldn't be
+# revealed.
 
 from critical import CriticalDensity
 from visualise import visualise
@@ -36,12 +47,12 @@ from minesweeper import Minesweeper
 # Performs many experiment to find critical density
 def main():
 
-    performance = True
+    performance = False
     quiet = False
     
     experiments = int(1e2)
-    trials = int(1e2)
-    cutoff = 1e5
+    trials = int(1e4)
+    cutoff = 1e6
     alpha = 0.65
     step = 0.3
     rho = 3 / 5
@@ -76,5 +87,5 @@ def see_ms():
     visualise(ms)
 
 
-#main()
-see_ms()
+main()
+#see_ms()
