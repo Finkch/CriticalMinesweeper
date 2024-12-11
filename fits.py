1321abe923bfb8eb49f5944a9ccbf48e5df56095
Finkch <skyler@thealdersons.org>
2024-12-11 15:22:18 -0800

    Added new fits: exponential, power, and ration, all plus 1

--- fits.py
+++ fits.py
@@ -6,6 +6,18 @@ def linear(x, m, b):
     return m * x + b
 
 def exponential(x, m, b):
-    return e ** (m * x + b)
+    return e ** (m * x + b) + 1
     # This is equivalent to...if m = ln(b)
     # m * e ** (m * x)
+
+def exponentialp1(x, m, b):
+    return e ** (m * x + b) + 1
+    # This is equivalent to...if m = ln(b)
+    # m * e ** (m * x)
+
+def powerp1(x, a, b):
+    return b * x ** a + 1
+
+def rationalp1(x, a, b):
+    return a / (x + b) + 1
+
